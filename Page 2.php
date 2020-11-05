85a00cab954c3fc4843ac39c6bdd0df47f87c943
benjamin31830 <73987338+benjamin31830@users.noreply.github.com>
2020-11-05 09:11:58 +0100

    Add files via upload

---
+++ Page 2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	 
+	<head>
+	    <metacharset="utf-8">
+	    <title>Page 2</title>
+	     <link rel="stylesheet" href="Deco.css">
+	     	   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+	<?php include("menu.php");?>
+
+	</head>
+
+	<body>
+		<center><h1>Usain Bolt</h1></center>
+		<center><img src="usainbolt2"
+     width="400"
+     height="250"></center>
+     	<center>
+     <p> Qui veut faire la course avec le sprinteur le plus rapide du monde ?Usain Bolt</p>
+      <p>Lightning Bolt dans sa tenue zébrée nous fait sa figure favorite.</p>
+      <p>Toctoc nous représente le coureur et sprinteur jamaïcain le plus célèbre du monde dans une œuvre riche en couleurs.</p>
+     	</center>
+
+   
+ 	</body>
+ </html>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php
+	echo'
+        <ul>
+        	<li><a href="page1.php">Accueil</a></li>
+        	<li><a href="page2.php">Présentation de loeuvre</a></li>
+       		<li><a href="page3.php">Nos coordonnées et autres CafArt</a></li>
+        	<li><a href="page4.php">Quizz</a></li>
+        </ul>'  
+?>
